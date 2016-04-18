c8c77a3b608a3a1ef4d99f42a07e0acbf859a09f
Nick Taylor <taylorn@udel.edu>
2016-04-18 15:33:52 +0000

    finished remove_duplicates

--- remove_duplicates/lib/uniques.rb
+++ remove_duplicates/lib/uniques.rb
@@ -1,7 +1,12 @@
 module Uniques
 
   def self.cleanup(array)
-    
+    uniques = []
+    array.each do |element|
+      uniques << element unless uniques.include? element
+    end
+
+    return uniques
   end
 
 end
