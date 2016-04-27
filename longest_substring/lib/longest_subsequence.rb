5a30c17e4206c84a13a14ddecb941aa94e6c7811
Nick Taylor <taylorn@udel.edu>
2016-04-27 13:15:22 +0000

    Finished longest_substring brute force solution

--- longest_substring/lib/longest_subsequence.rb
+++ longest_substring/lib/longest_subsequence.rb
@@ -1,7 +1,52 @@
 module LongestSubsequence
 
 	def self.find(first, second)
+    return nil if first.nil? || second.nil?
 
+    curr_sequence = {
+      :length => 0,
+      :chars => []
+    }
+
+    first_possibles = first.chars.permutation.to_a
+    second_possibles = second.chars.permutation.to_a
+
+    first_possibles.each do |charr1|
+      second_possibles.each do |charr2|
+        new_sequence = LongestSubsequence.shared_sequence(charr1, charr2)
+        if new_sequence[:length] > curr_sequence[:length]
+          curr_sequence = new_sequence
+        end
+      end
+    end
+
+    return curr_sequence[:length] == 0 ? nil : curr_sequence[:chars].sort.join('')
 	end
+
+  def self.shared_sequence(charr1, charr2)
+    return nil if charr1.nil? || charr2.nil?
+
+    sequence = {
+      :length => 0,
+      :chars => []
+    }
+
+    if charr1.size > charr2.size
+      iterated = charr2
+      compared = charr1
+    else
+      iterated = charr1
+      compared = charr2
+    end
+
+    iterated.each_with_index do |elem, i|
+      if elem == compared[i]
+        sequence[:length] += 1
+        sequence[:chars] << elem
+      end
+    end
+
+    return sequence
+  end
 
 end
--- longest_substring/spec/longest_subsequence_spec.rb
+++ longest_substring/spec/longest_subsequence_spec.rb
@@ -2,21 +2,41 @@ require 'spec_helper'
 
 RSpec.describe LongestSubsequence, type: :module do
   describe "LongestSubsequence#find" do
-    
+
     it "should result in nil when given nil" do
       longest = LongestSubsequence.find(nil, nil)
       expect(longest).to eq nil
     end
-    
+
     it "should result in nil when given no common subsequences" do
       longest = LongestSubsequence.find("a", "b")
       expect(longest).to eq nil
     end
-    
+
     it "should return the longest subsequence when given a short string" do
       longest = LongestSubsequence.find("ABCDAF", "ACBCF")
       expect(longest.chars.sort.join).to eq "ABCF"
     end
-    
+
+  end
+
+  describe "LongestSubsequence.continuous_sequence_length" do
+
+    it "should result in nil when given nil" do
+      sequence = LongestSubsequence.shared_sequence(nil, nil)
+      expect(sequence).to eq nil
+    end
+
+    it "should result in 0 when no sequence length is the same" do
+      sequence = LongestSubsequence.shared_sequence("cats".chars, "dog".chars)
+      expect(sequence[:length]).to eq 0
+    end
+
+    it "should result in the length of common sequence length for normal input" do
+      sequence = LongestSubsequence.shared_sequence("stocks".chars, "store".chars)
+      expect(sequence[:length]).to eq 3
+      expect(sequence[:chars].join('')).to eq "sto"
+    end
+
   end
 end
