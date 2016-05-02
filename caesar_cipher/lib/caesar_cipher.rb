2b0981ff0b69d1a198dbf7f9466a281cb312c705
Nick Taylor <taylorn@udel.edu>
2016-05-02 17:01:20 +0000

    Finish caesar cipher

--- caesar_cipher/lib/caesar_cipher.rb
+++ caesar_cipher/lib/caesar_cipher.rb
@@ -1,6 +1,36 @@
 class String
   def rot13
-    # %, .ord method
-    self
+    # "Dictionaries"
+    lowerCase = ("a".."z").to_a
+    upperCase = ("A".."Z").to_a
+
+    # Setup
+    chars = self.chars
+    rotated = []
+
+    # Perform the rotation
+    chars.each do |char|
+      if lowerCase.include?(char)
+        index = lowerCase.index(char)
+        arr = lowerCase
+      elsif upperCase.include?(char)
+        index = upperCase.index(char)
+        arr = upperCase
+      elsif char == " "
+        rotated << char
+        next
+      else
+        raise StandardError, "Disallowed character."
+      end
+
+      rotated_index = index + 13
+      if rotated_index > 25
+        rotated_index -= 26
+      end
+
+      rotated << arr[rotated_index]
+    end
+
+    rotated.join('')
   end
 end
